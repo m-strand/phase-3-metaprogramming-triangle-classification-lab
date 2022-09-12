453943b35fc9dd0e0c409ca1a055a6b7ba20fcf5
Brooke <brooke.strand007@gmail.com>
2022-09-11 21:56:10 -0400

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize(l1, l2, l3)
+    @l1=l1
+    @l2=l2
+    @l3=l3
+  end
+
+  def kind
+    if (l1 <= 0) || (l2 <= 0) || (l3 <=0) 
+      raise TriangleError
+      elsif (l1 + l2 <= l3) || (l3 + l2 <= l1) || (l3 + l1 <= l2)
+      raise TriangleError
+    end
+   
+    if l1 == l2 && l2 == l3
+      return :equilateral 
+    elsif l1 == l2 || l3 == l2 || l3 == l1 
+      return :isosceles
+    elsif l1 != l2 && l2 != l3
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
